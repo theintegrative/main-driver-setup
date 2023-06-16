9e5794a1e1cd69df2f227434bc415c6529b28eae
theintegrative <theintegrative@theintegrative-thin.home>
2023-06-16 17:56:34 +0200

    add initial logins to get access to git repos

--- 0.sh
+++ 0.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 echo "Hello world"
+SSH_PRIV_KEY="~/.ssh/id_ed25519"
+SSH_PUB_KEY="$SSH_PRIV_KEY.pub"
+sudo zypper in -y keepassxc ansible git gh glab
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+ssh-keygen -t ed25519 -f "$SSH_PRIV_KEY"
+gh auth login -h github.com -p ssh -w -s admin:public_key
+glab auth login -h gitlab.com 
+glab ssh-key add -t "main driver" "$SSH_PUB_KEY"
