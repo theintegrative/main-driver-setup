6e82ce99f88d5bfbf4e7c305abbd7e7190767959
theintegrative <theintegrative@gmail.com>
2023-06-18 15:02:37 +0200

    add pip isntall and aditional ansible packages for zypper to work in ansible

--- 0.sh
+++ 0.sh
@@ -2,7 +2,7 @@
 echo "Hello world"
 SSH_PRIV_KEY="$HOME/.ssh/id_ed25519"
 SSH_PUB_KEY="$SSH_PRIV_KEY.pub"
-sudo zypper --non-interactive in -y keepassxc ansible git gh glab
+sudo zypper --non-interactive in -y python3-pip keepassxc ansible git gh glab
 pip install ansible
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
