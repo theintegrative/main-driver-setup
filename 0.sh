bad661eefa795d5ae594599c5c0a4bddaa80ae74
theintegrative <theintegrative@gmail.com>
2023-06-18 13:35:04 +0200

    make zypper non interactive

--- 0.sh
+++ 0.sh
@@ -2,7 +2,7 @@
 echo "Hello world"
 SSH_PRIV_KEY="$HOME/.ssh/id_ed25519"
 SSH_PUB_KEY="$SSH_PRIV_KEY.pub"
-sudo zypper in -y keepassxc ansible git gh glab
+sudo zypper --non-interactive in -y keepassxc ansible git gh glab
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 ssh-keygen -t ed25519 -f "$SSH_PRIV_KEY"
