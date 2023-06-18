1bc7dadc7eb8a7d15587066701d0ca6c61207ffe
theintegrative <theintegrative@gmail.com>
2023-06-18 14:22:11 +0200

    remove sudo

--- 0.sh
+++ 0.sh
@@ -13,5 +13,5 @@ else
 	glab auth login -h gitlab.com 
 	glab ssh-key add -t "main driver" "$SSH_PUB_KEY"
 fi
-sudo ansible-galaxy install -r ansible/requirements.yml
+ansible-galaxy install -r ansible/requirements.yml
 ansible-playbook -i localhost, --connection="local" -b ansible/configure.yml
